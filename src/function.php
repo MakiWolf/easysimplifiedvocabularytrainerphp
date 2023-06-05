1029f6eef0120b42443419ecd440589fb78b4824
MakiWolf <makiwolf@outlook.at>
2023-06-05 12:48:39 +0200

    improvements

--- src/function.php
+++ src/function.php
@@ -1,45 +1,52 @@
 <?php
-function viewtables($s, $e, $servername, $dbname, $charset, $username, $password, $table, $id){
-//$s and $e are used for start and end by some special queries
 
-try {
-    
-     $pdo = new PDO("mysql:host=$servername;dbname=$dbname;$charset", $username, $password);
-    
-     if($table == "vocabulary"){
-    	$statement = $pdo->prepare("SELECT * FROM vocabulary WHERE ID BETWEEN :s AND :e");
-    	$statement->execute(array('s' => $s, 'e' => $e));
-        echo "<center><table border><tr><td>ID</td><td>language1</td><td>language2</td><td></td></tr>";
-   	 while ($row = $statement->fetch()) {
-       		 echo "<tr><td>".$row['ID']."</td><td>".htmlspecialchars($row['language1'])."</td><td>".htmlspecialchars($row['language2'])."</td><td><a href ='editvocabulary.php?id=".$row['ID']."'>edit</a></td></tr>";
-   	 }
-      echo "</table></center>";
-    }
-    elseif($table == "editvocabulary"){
-    	$statement = $pdo->prepare("SELECT * FROM vocabulary WHERE ID = ".$id."");
-    	$statement->execute();
-    	
-       while ($row = $statement->fetch() ) {
-       		    echo "
-        <p><b>edit:</b></p>
-        <form method=\"post\" action=\"editvocabulary_submit.php\">
-	    <input type=\"hidden\" name=\"id\" value=\"" .$id. "\"><br>
-	    <span>language1: </span><input type=\"text\" name=\"language1\" value=\"".$row["language1"]."\"><br>
-	    <span>language2: </span><input type=\"text\" name=\"language2\" value=\"".$row["language2"]."\"><br>	
-	    <input type='submit' data-loading-text='save' value='  save  '>
-	    </form> ";
-    
-      
-        }
-	
-     
-        }
+function viewtablevocabulary($s, $e, $connstring)
+{
+	//$s and $e are used for start and end by some special queries
 
-   	
-    
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
+	try {
+
+		$pdo = new PDO($connstring, $_SESSION["usern"], $_SESSION["passwd"]);
+
+		$statement = $pdo->prepare("SELECT * FROM vocabulary WHERE ID BETWEEN :s AND :e");
+		$statement->execute(array('s' => $s, 'e' => $e));
+		echo "<center><table border><tr><td>ID</td><td>language1</td><td>language2</td><td></td></tr>";
+		while ($row = $statement->fetch()) {
+			echo "<tr><td>" . $row['ID'] . "</td><td>" . htmlspecialchars($row['language1']) . "</td><td>" . htmlspecialchars($row['language2']) . "</td><td><a href ='editvocabulary.php?id=" . $row['ID'] . "'>edit</a></td></tr>";
+		}
+		echo "</table></center>";
+	} catch (PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
+	$pdo = null;
 }
 
-$pdo = null;
+
+
+
+function editvocabulary($connstring, $id)
+{
+
+	try {
+
+		$pdo = new PDO($connstring, $_SESSION["usern"], $_SESSION["passwd"]);
+
+
+		$statement = $pdo->prepare("SELECT * FROM vocabulary WHERE ID = " . $id . "");
+		$statement->execute();
+
+		while ($row = $statement->fetch()) {
+			echo "
+			<p><b>edit:</b></p>
+			<form method=\"post\" action=\"editvocabulary_submit.php\">
+			<input type=\"hidden\" name=\"id\" value=\"" . $id . "\"><br>
+			<span>language1: </span><input type=\"text\" name=\"language1\" value=\"" . $row["language1"] . "\"><br>
+			<span>language2: </span><input type=\"text\" name=\"language2\" value=\"" . $row["language2"] . "\"><br>	
+			<input type='submit' data-loading-text='save' value='  save  '>
+			</form> ";
+		}
+	} catch (PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
+	$pdo = null;
+}
